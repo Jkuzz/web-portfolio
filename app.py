8fff1b80280f449596eec7ac09ce8d5a09d59840
Jkuzz <honza.ku@gmail.com>
2021-12-26 11:09:48 +0100

    Iniitial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, send_file
+import glob
+app = Flask(__name__)
+
+
+routing_dict = {
+    'home': 'home.html',
+    'contact': 'contact.html',
+    'prices': 'prices.html',
+    'test': 'test.html',
+}
+
+
+@app.route('/resources/<resource>')
+def send_js(resource):
+    if '..' in resource or '//' in resource:
+        return None
+    return send_file(f'./resources/{resource}')
+
+
+@app.route('/portfolio')
+def render_portfolio():
+    pictures = glob.glob('static/portfolio/*.jpg')
+    return render_template('portfolio.html', pictures=pictures)
+
+
+@app.route('/<page>')
+def render_page(page):
+    if page not in routing_dict.keys():
+        return None
+    return render_template(routing_dict[page])
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/portfolio.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imageAlbum = document.querySelector(".album");
+    const links = imageAlbum.querySelectorAll("a");
+    const lightboxModal = document.getElementById("photoModal");
+    const bsModal = new bootstrap.Modal(lightboxModal);
+
+    for (const link of links) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const currentImg = link.querySelector("img");
+            const lightboxCarousel = document.getElementById("lightboxCarousel");
+            if (lightboxCarousel) {
+                const parentCol = link.parentElement.parentElement;
+                const index = [...parentCol.parentElement.children].indexOf(parentCol);
+                const bsCarousel = new bootstrap.Carousel(lightboxCarousel);
+                bsCarousel.to(index);
+            } else {
+                createCarousel(currentImg);
+            }
+            bsModal.show();
+        });
+    }
+});
+
+function createCarousel(img) {
+    const modalBody = document.querySelector(".modal-body .container-fluid");
+    modalBody.innerHTML = `
+    <div id="lightboxCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="false">
+      <div class="carousel-inner">
+        ${createSlides(img)}
+      </div> 
+      <button class="carousel-control-prev" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="prev">
+       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+       <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    `;
+}
+
+function createSlides(img) {
+    let markup = "";
+    const currentImgSrc = img.getAttribute("src");
+
+    const imageAlbum = document.querySelector(".album");
+    const allImages = imageAlbum.querySelectorAll("img");
+
+    for (const img of allImages) {
+        const imgSrc = img.getAttribute("src");
+        const imgAlt = img.getAttribute("alt");
+        const imgCaption = img.getAttribute("data-caption");
+
+        markup += `
+    <div class="carousel-item${currentImgSrc === imgSrc ? " active" : ""}">
+      <img src=${imgSrc} alt=${imgAlt}>
+      ${imgCaption ? createCaption(imgCaption) : ""}
+    </div>
+    `;
+    }
+
+    return markup;
+}
+
+function createCaption(caption) {
+    return `<div class="carousel-caption">
+     <p class="m-0">${caption}</p>
+    </div>`;
+}
